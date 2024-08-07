079f61c18e505a2b939b2661a44a43b020389c76
Ileri Arowolo <54251768+Ileri-Arowolo@users.noreply.github.com>
2024-08-07 23:13:03 +0100

    New feature added

--- vcs/deploy_mastercard.sh
+++ vcs/deploy_mastercard.sh
@@ -1,3 +1,5 @@
 We are the NO 1 payment solution in the world
 We deliver fast money transfer
 WE have processed over 1000 payments successfully
+
+#This is a new feature is being added to the application 
